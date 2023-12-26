845b1d188bf53aa4c230c1548254581fceb059cb
EnzoAMarco <enzom945@gmail.com>
2023-12-26 00:05:34 -0300

    first commit

---
+++ src/main/java/com/enzomarco/gestion/concesionaria/GestionConcesionaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.enzomarco.gestion.concesionaria;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestionConcesionaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
